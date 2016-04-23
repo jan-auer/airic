e2dd3c4731cbc118ea6f58e3291938a25491402b
Jan Michael Auer <mail@jauer.org>
2016-04-23 10:36:10 +0200

    Add the Spring project stub.

---
+++ server/src/main/java/org/spaceapps/aircheck/server/Application.java
@@ -0,0 +1,43 @@
+package org.spaceapps.aircheck.server;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class Application {
+
+	@Autowired
+	private Environment env;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
+
+	@Bean
+	public ReloadableResourceBundleMessageSource getMessageSource() {
+		final ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+		source.setFallbackToSystemLocale(false);
+		source.setBasenames(
+				"classpath:common"
+		);
+
+		if (env.acceptsProfiles("dev")) {
+			source.setCacheSeconds(0);
+		} else {
+			source.setCacheSeconds(-1);
+		}
+
+		return source;
+	}
+
+}
